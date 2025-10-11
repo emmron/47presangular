8aaf2a2333c2bbd63b583d7cd6f533337b33c534
Emmett Hoolahan <emmetthoolahan@gmail.com>
2025-10-12 00:35:26 +0800

    feat: add curated timeline and topic views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'timeline'
+  },
+  {
+    path: 'timeline',
+    loadComponent: () =>
+      import('./routes/timeline/timeline.component').then(m => m.TimelineComponent)
+  },
+  {
+    path: 'topics/:slug',
+    loadComponent: () =>
+      import('./routes/topic-page/topic-page.component').then(m => m.TopicPageComponent)
+  },
+  {
+    path: 'news',
+    loadComponent: () =>
+      import('./components/news-feed/news-feed.component').then(m => m.NewsFeedComponent)
+  },
+  {
+    path: '**',
+    redirectTo: 'timeline'
+  }
+];
