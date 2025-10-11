4684a296da7a5bc3da148bf2eef6407fca571c7c
Emmett Hoolahan <emmetthoolahan@gmail.com>
2025-10-12 04:34:33 +0800

    chore: restore layout module provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { LayoutModule } from './layout/layout.module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(LayoutModule),
     provideRouter(routes),
     provideHttpClient()
   ]
