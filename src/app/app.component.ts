db52e4af813c3f6e158abca35489f4efb5c69828
Emmett Hoolahan <emmetthoolahan@gmail.com>
2025-10-12 04:20:09 +0800

    Align AppComponent with shell-based routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { ShellComponent } from './layout/shell/shell.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ShellComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
